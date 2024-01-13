c8ed2c7146c4daaccda422a2b84ce5e63d62a8f0
jefryalbukhori23 <jefryalbukhori23@gmail.com>
2024-01-13 09:30:44 +0700

    create AuthCOntroller for login and logout function

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -3,8 +3,60 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|email|unique:users',
+            'password' => 'required',
+        ]);
+
+        // $data = [
+        //     'email' => $request->email,
+        //     'password' => $request->password,
+        // ];
+
+        // if(Auth::attempt($data)){
+        //     return redirect('/dashboard_admin');
+        // }else{
+        //     return back();  
+        // }
+
+        if ($validator->fails()) {
+            return response()->json(['success' => false, 'errors' => $validator->errors()], 422);
+        }
+
+        // dd($request->all());
+
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (Auth::attempt($credentials)) {
+                return response()->json(['success' => true, 'user' => Auth::user()]);
+            } else {
+                throw ValidationException::withMessages([
+                    'email' => __('auth.failed'),
+                ]);
+            }
+        } catch (ValidationException $e) {
+            return response()->json(['success' => false, 'errors' => $e->errors()], 422);
+        }
+
+    }
+
+    public function logout(Request $request)
+    {
+        Auth::logout();
+
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        return redirect('/'); // Ganti dengan halaman yang ingin Anda tampilkan setelah logout
+    }
 }
