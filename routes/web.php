53e12d4dfd1ce82ad48b5ea9d689f802cda11c39
SandyPrida <sandy752005@gmail.com>
2024-01-14 20:58:26 +0700

    sandy

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\qrController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +15,11 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('Auth.login');
 });
+
+Route::get('/qrCode', [qrController::class, 'index']);
+Route::get('/absen', [qrController::class, 'absen']);
 
 Route::group(['middleware' => ['auth', 'hakakses:admin']], function () {
 
