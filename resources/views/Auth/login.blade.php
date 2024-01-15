fa31bca95dc50648a91c39c7bf403d76194e3adb
SandyPrida <sandy752005@gmail.com>
2024-01-15 10:26:08 +0700

    Login fe

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -4,9 +4,28 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous"> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{asset('assets/customUser.css')}}">
+    <script>
+        tailwind.config = {
+            theme: {
+                extend: {},
+                colors: {
+                    white: '#FFF',
+                    color1: '#1D60A2',
+                    color2: '#9E9E9E',
+                    color3: '#76B5E3',
+                }
+            }
+        }
+    </script>
     <style>
+        * {
+            box-sizing: border-box;
+        }
         .fullscreen-overlay {
             display: none;
             position: fixed;
@@ -37,26 +56,38 @@
             Loading...
         </div>
     </div>
-    <div class="container">
-        <div class="d-flex">
-            <div class=""></div>
-            <div class="d-flex flex-column align-items-center justify-content-center">
-                <p>Login</p>
-                <form id="form-login" action="" class="d-flex flex-column align-items-center justify-content-center">
-                    @csrf
-                    <label for="">Masukkan Email</label>
-                    <input type="email" name="email" id="email">
-                    <label for="">Masukkan password</label>
-                    <input type="password" name="password" id="password">
-                    <button type="submit" class="btn btn-success">
-                        Login
-                    </button>
-                </form>
+    <div class="flex h-screen items-center justify-center">
+        <div class="hidden sm:flex w-[50%] h-full items-center justify-center">
+            <div class="mx-[50px]">
+                <img src="{{asset('assets/image/image1.png')}}" alt="">
             </div>
+        </div>
+        <div class="w-full sm:w-[50%] h-full flex flex-col items-center justify-center bg-color3">
+            <p class="text-color1 text-[36px] font-bold Poppins mb-[70px]">Login</p>
+            <form id="form-login" action="" class="w-[90%] lg:w-[60%] flex flex-col items-center justify-center">
+                @csrf
+                <div class="relative w-full flex flex-col mb-4">
+                    <div class="absolute bottom-5 text right-3">
+                        <i class="fa-solid fa-user"></i>
+                    </div>
+                    <label for="" class="Inter text-[16px] text-color1 mb-2">Masukkan Email</label>
+                    <input class="p-3 w-full Inter font-normal border border-color1 bg-white outline-none focus:outline-offset-0 focus:outline-color1 h-[64px] rounded-[10px]" placeholder="Username" type="email" name="email" id="email">
+                </div>
+                <div class="relative w-full flex flex-col mb-4">
+                    <div class="absolute bottom-5 text right-3">
+                        <i class="fa-solid fa-eye"></i>
+                    </div>
+                    <label for="" class="Inter text-[16px] text-color1 mb-2">Masukkan password</label>
+                    <input class="p-3 w-full Inter font-normal border border-color1 bg-white outline-none focus:outline-offset-0 focus:outline-color1 h-[64px] rounded-[10px]" placeholder="Password" type="password" name="password" id="password">
+                </div>
+                <button type="submit" class="w-full bg-color1 py-4 text-[19px] font-medium text-white rounded-[10px]">
+                    Login
+                </button>
+            </form>
         </div>
     </div>
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script> -->
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
